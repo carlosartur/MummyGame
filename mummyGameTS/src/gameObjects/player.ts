1659ba673c62a56a0a0c21ed43bbbecd17345b86
Thomas Souza <thomas.aes.31@hotmail.com>
2016-10-22 16:21:55 -0200

    Classe de movimento
    Cria classe responsavel pelo movimento tendo funções basicas de movimento
    para um plataformer.
    
    Remove codigo de movimento que havia na classe Player e utiliza a classe
    Movement no lugar.

--- mummyGameTS/src/gameObjects/player.ts
+++ mummyGameTS/src/gameObjects/player.ts
@@ -4,6 +4,9 @@
         cursors: any;
         level: Level01;
         game: Phaser.Game;
+        movement: Movement;
+        movementSpeed: number;
+        stoppedFrame: number;
 
         constructor(game: Phaser.Game, level: Level01, x: number, y: number) {
 
@@ -20,6 +23,7 @@
 
             this.animations.add('left', [8, 9, 10, 11], 10, true);
             this.animations.add('right', [8, 9, 10, 11], 10, true);
+            this.stoppedFrame = 4;
 
             game.add.existing(this);
             // Physics
@@ -27,29 +31,17 @@
             this.body.collideWorldBounds = true;
             this.body.setCircle(20);
             this.anchor.set(0.5);
+
+            this.movementSpeed = 100;
+            this.movement = new Movement(game, this);
         }
 
         update() {
             this.body.velocity.x = 0;
             var hitPlatforms = this.game.physics.arcade.collide(this, this.level.platforms);
+
+            this.movement.arowsMove(hitPlatforms);
             this.game.physics.arcade.overlap(this, this.level.stars, this.collectStar, null);
-
-            if (this.cursors.left.isDown) {
-                this.scale.set(-1, 1);
-                this.body.velocity.x = -150;
-                this.animations.play('left');
-            } else if (this.cursors.right.isDown) {
-                this.scale.set(1, 1);
-                this.body.velocity.x = 150;
-                this.animations.play('right');
-            } else {
-                this.animations.stop;
-                this.frame = 4;
-            }
-
-            if (this.cursors.up.isDown && this.body.touching.down && hitPlatforms) {
-                this.body.velocity.y = -300;
-            }
         }
 
         collectStar(player, star) {
---
+++ mummyGameTS/src/utils/movement.ts
@@ -0,0 +1,44 @@
+﻿module mummyGameTS.Client {
+
+    export class Movement{
+        object: any;
+        arows: any;
+        
+        constructor(game: Phaser.Game, object: any) {
+            this.object = object;
+            this.arows = game.input.keyboard.createCursorKeys();
+        }
+
+        arowsMove(hitPlatform: boolean)
+        {
+            console.log(this.object);
+
+            if (this.arows.left.isDown) {
+                this.moveLeft();
+            } else if (this.arows.right.isDown) {
+                this.moveRight();
+            } else {
+                this.object.animations.stop;
+                this.object.frame = this.object.stoppedFrame;
+            }
+
+            if (this.arows.up.isDown && this.object.body.touching.down && hitPlatform) {
+                this.object.body.velocity.y = -300;
+            }
+        }
+
+        moveLeft() {
+            this.object.scale.set(-1, 1);
+            this.object.body.velocity.x = - this.object.movementSpeed;
+            this.object.animations.play('left');
+        }
+
+        moveRight() {
+            this.object.scale.set(1, 1);
+            this.object.body.velocity.x = this.object.movementSpeed;
+            this.object.animations.play('right');
+        }
+
+    }
+
+}
